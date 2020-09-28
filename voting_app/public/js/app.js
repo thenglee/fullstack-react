d397f9f948de8abc32c7fe0051e79e3bb6f0a001
Tan Theng Lee <thengleetan@gmail.com>
2020-09-28 16:13:26 +0800

    Propagate upvote event to parent component

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,4 +1,8 @@
 class ProductList extends React.Component {
+  handleProductUpVote(productId) {
+    console.log(productId + ' was upvoted')
+  }
+
   render() {
     const products = Seed.products.sort((a, b) => b.votes - a.votes)
 
@@ -12,6 +16,7 @@ class ProductList extends React.Component {
         votes={product.votes}
         submitterAvatarUrl={product.submitterAvatarUrl}
         productImageUrl={product.productImageUrl}
+        onVote={this.handleProductUpVote}
       />
     ))
 
@@ -20,6 +25,16 @@ class ProductList extends React.Component {
 }
 
 class Product extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.handleUpVote = this.handleUpVote.bind(this)
+  }
+
+  handleUpVote() {
+    this.props.onVote(this.props.id)
+  }
+
   render() {
     return (
       <div className="item">
@@ -28,7 +43,7 @@ class Product extends React.Component {
         </div>
         <div className="middle aligned content">
           <div className="header">
-            <a>
+            <a onClick={this.handleUpVote}>
               <i className="large caret up icon" />
             </a>
             {this.props.votes}
